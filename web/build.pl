bf14a5c6d3754e760355d39c9b81601304caf4c0
vance <vance@1e5ffdc8-eadd-0310-9daa-9cb4117fe24b>
2006-03-14 06:26:41 +0000

    r290 didn't actually fix the problem - the output is already CGI-escaped, so > is actually &gt;
    Vance
    
    
    
    git-svn-id: file:///home/svn/build-farm/trunk@291 1e5ffdc8-eadd-0310-9daa-9cb4117fe24b

--- web/build.pl
+++ web/build.pl
@@ -875,16 +875,17 @@ sub print_log_pretty() {
 	       (.*?)
 	       ACTION\ (PASSED|FAILED):\ ([\w\-]+)
 	     }{make_action_html($1, $2, $id++, $3)}exgs;
-  
+
+  # $log is already CGI-escaped, so handle '>' in test name by handling &gt;
   $log =~ s{
 	      --==--==--==--==--==--==--==--==--==--==--.*?
-	      Running\ test\ ([\w\-=,_:\ /.>]+)\ \(level\ (\d+)\ (\w+)\).*?
+	      Running\ test\ ([\w\-=,_:\ /.&;]+)\ \(level\ (\d+)\ (\w+)\).*?
 	      --==--==--==--==--==--==--==--==--==--==--
               (.*?)
 	      ==========================================.*?
 	      TEST\ (FAILED|PASSED|SKIPPED):(\ \(status\ (\d+)\))?.*?
 	      ==========================================\s+
-	     }{make_test_html(util::cgi_escape($1), $4, $id++, $5)}exgs;
+	     }{make_test_html($1, $4, $id++, $5)}exgs;
 
 
 	print "<tt><pre>" .join('', $log) . "</pre></tt><p>\n";
