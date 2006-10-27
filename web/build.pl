b3e0c4cc689c8aba8d1a9956265a5ae2864a08e4
tridge <tridge@1e5ffdc8-eadd-0310-9daa-9cb4117fe24b>
2006-10-27 07:08:08 +0000

    fixed display of several tests (mainly RPC-SECRETS)
    git-svn-id: file:///home/svn/build-farm/trunk@462 1e5ffdc8-eadd-0310-9daa-9cb4117fe24b

--- web/build.pl
+++ web/build.pl
@@ -896,13 +896,13 @@ sub print_log_pretty() {
   # $log is already CGI-escaped, so handle '>' in test name by handling &gt;
   $log =~ s{
 	      --==--==--==--==--==--==--==--==--==--==--.*?
-	      Running\ test\ ([\w\-=,_:\ /.&;]+)\ \(level\ (\d+)\ (\w+)\).*?
+	      Running\ test\ ([\w\-=,_:\ /.&;]+).*?
 	      --==--==--==--==--==--==--==--==--==--==--
               (.*?)
 	      ==========================================.*?
-	      TEST\ (FAILED|PASSED|SKIPPED):(\ \(status\ (\d+)\))?.*?
+	      TEST\ (FAILED|PASSED|SKIPPED):.*?
 	      ==========================================\s+
-	     }{make_collapsible_html('test', $1, $4, $id++, $5)}exgs;
+	     }{make_collapsible_html('test', $1, $2, $id++, $3)}exgs;
 
   print "<tt><pre>" .join('', $log) . "</pre></tt><p>\n";
 }
