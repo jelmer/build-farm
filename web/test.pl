2cf84c36158a82f722e7a4fc6c4a341d1f7c1e53
jelmer <jelmer@1e5ffdc8-eadd-0310-9daa-9cb4117fe24b>
2007-02-11 13:29:29 +0000

    Fix test view, use more perl functions.
    git-svn-id: file:///home/svn/build-farm/trunk@512 1e5ffdc8-eadd-0310-9daa-9cb4117fe24b

--- web/test.pl
+++ web/test.pl
@@ -20,14 +20,14 @@ sub show_summary($)
 {
 	my $tree = shift;
 
-	print "<table>\n";
+	print $cgi->start_table();
 	print "<thead><tr><td>Test</td><td>Breakages</td><td>Hosts</td></tr></thead>\n";
 	print "<tbody>\n";
 
 	my $failed = {};
 	my $success = {};
 
-	my $resultref = $dbh->selectall_arrayref("SELECT test_run.test AS test, build.host AS host, build.compiler AS compiler, test_run.result AS testresult, build.revision AS revision, build.checksum AS checksum FROM build, test_run WHERE build.tree = ? AND test_run.build = build.id GROUP BY test, host, compiler ORDER BY revision", undef, $tree);
+	my $resultref = $dbh->selectall_arrayref("SELECT test_run.test AS test, build.host AS host, build.compiler AS compiler, test_run.result AS testresult, build.revision AS revision, build.checksum AS checksum FROM build, test_run WHERE build.tree = ? AND test_run.build = build.id GROUP BY test, host, compiler ORDER BY revision DESC", undef, $tree);
 	foreach (@$resultref) {
 		unless (defined($failed->{$_->[0]})) { $failed->{$_->[0]} = []; }
 		unless (defined($success->{$_->[0]})) { $success->{$_->[0]} = 0; }
@@ -44,7 +44,9 @@ sub show_summary($)
 		
 		my $numfails = $#{$failed->{$_}}+1;
 		
-		printf "<tr><td>$_</td><td>$numfails (%.2f%%)</td><td>", ($numfails / ($numfails+$success->{$_}) * 100.0);
+		print "<tr>" . $cgi->td($cgi->a({-href=>"/test.pl?test=".uri_escape($_)}, $_));
+
+		printf "<td>$numfails (%.2f%%)</td><td>", ($numfails / ($numfails+$success->{$_}) * 100.0);
 
 		foreach (@{$failed->{$_}}) {
 			print $cgi->a({-href=>"/test.pl?build=".uri_escape($_->[5]).";test=".uri_escape($_->[0])}, "$_->[1]/$_->[2]($_->[4])"). " ";
@@ -54,7 +56,7 @@ sub show_summary($)
 	}
 
 	print "</tbody>\n";
-	print "</table>\n";
+	print $cgi->end_table;
 }
 
 sub show_test_host($$)
@@ -62,16 +64,16 @@ sub show_test_host($$)
 	my ($test, $build) = @_;
 
 	my @resultref = $dbh->selectrow_array("SELECT test_run.output FROM test_run, build WHERE test_run.build = build.id AND build.checksum = ? AND test_run.test = ? ORDER BY build.revision LIMIT 1", undef, $build, $test);
-	print "$resultref[0]\n";
+	print $cgi->pre($resultref[0]);
 }
 
 sub show_test($)
 {
 	my ($test) = @_;
-	my $resultref = $dbh->selectall_arrayref("SELECT build.host, build.compiler, build.revision, test_run.output FROM test_run, build WHERE build.id = test_run.build AND test_run.test = ?", undef, $test);
+	my $resultref = $dbh->selectall_arrayref("SELECT build.host, build.compiler, build.revision, test_run.output FROM test_run, build WHERE build.id = test_run.build AND test_run.test = ? ORDER BY test_run.result", undef, $test);
 	foreach (@$resultref) {
-		print "<h2>$_->[0]</h2>";
-		print "$_->[2]\n";
+		print $cgi->h2($_->[0]);
+		print $cgi->pre($_->[3]);
 	}
 }
 
