3cc5b18fff9390cffb194386def35000a0b8a82f
jelmer <jelmer@1e5ffdc8-eadd-0310-9daa-9cb4117fe24b>
2007-02-11 13:11:13 +0000

    Track build age, build stage order
    git-svn-id: file:///home/svn/build-farm/trunk@510 1e5ffdc8-eadd-0310-9daa-9cb4117fe24b

--- fill_db.pl
+++ fill_db.pl
@@ -9,12 +9,13 @@ use DBI;
 use Digest::SHA1 qw(sha1_hex);
 use strict;
 use util;
+use File::stat;
 
 my $dbh = DBI->connect( "dbi:SQLite:data.dbl" ) || die "Cannot connect: $DBI::errstr";
 
-$dbh->do("CREATE TABLE build ( id integer primary key autoincrement, tree text, revision text, host text, compiler text, checksum text );");
-$dbh->do("CREATE TABLE test_run ( build int, test text, result text, output text);");
-$dbh->do("CREATE TABLE build_stage_run ( build int, action text, result text, output text);");
+$dbh->do("CREATE TABLE IF NOT EXISTS build ( id integer primary key autoincrement, tree text, revision text, host text, compiler text, checksum text, age int );");
+$dbh->do("CREATE TABLE IF NOT EXISTS test_run ( build int, test text, result text, output text);");
+$dbh->do("CREATE TABLE IF NOT EXISTS build_stage_run ( build int, action text, result text, output text, num int);");
 
 foreach my $logfn (@ARGV) {
 	if (not -f $logfn) {
@@ -35,9 +36,9 @@ foreach my $logfn (@ARGV) {
 	print "$logfn\n";
 
 	my ($rev) = ($data =~ /BUILD REVISION: ([^\n]+)/);
-	my $st = $dbh->prepare("INSERT INTO build (tree, revision, host, compiler, checksum) VALUES (?, ?, ?, ?, ?)");
-	# FIXME: add build time
-	$st->execute($tree, $rev, $host, $compiler, $checksum);
+	my $st = $dbh->prepare("INSERT INTO build (tree, revision, host, compiler, checksum, age) VALUES (?, ?, ?, ?, ?, ?)");
+	my $stat = stat($logfn);
+	$st->execute($tree, $rev, $host, $compiler, $checksum, $stat->mtime);
 	my $build = $dbh->func('last_insert_rowid');
 
 	$st = $dbh->prepare("INSERT INTO test_run (build, test, result, output) VALUES ($build, ?, ?, ?)");
@@ -53,10 +54,12 @@ foreach my $logfn (@ARGV) {
 		$st->execute($1, $3, $2);
 	}
 
-	$st = $dbh->prepare("INSERT INTO build_stage_run (output, build, action, result) VALUES (?, $build, ?, ?);");
+	$st = $dbh->prepare("INSERT INTO build_stage_run (output, build, action, result, num) VALUES (?, $build, ?, ?, ?);");
 
+	my $order = 0;
 	while ($data =~ /(.*?)?ACTION (FAILED|PASSED): ([^\n]+)/sg) {
-		$st->execute($1, $3, $2);
+		$st->execute($1, $3, $2, $order);
+		$order++;
 	}
 	$st->finish();
 }
