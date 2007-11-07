27e15bb6a14233ec5dc023f1c10f7354163d1747
metze <metze@1e5ffdc8-eadd-0310-9daa-9cb4117fe24b>
2007-11-07 09:12:41 +0000

    whitespace cleanup
    metze
    
    
    git-svn-id: file:///home/svn/build-farm/trunk@707 1e5ffdc8-eadd-0310-9daa-9cb4117fe24b

--- web/data.pm
+++ web/data.pm
@@ -64,23 +64,22 @@ sub new($) {
 
 sub cache_fname($$$$$)
 {
-    my ($self, $tree, $host, $compiler, $rev) = @_;
-    if ($rev) {
-	    return "$CACHEDIR/oldrevs/build.$tree.$host.$compiler-$rev";
-    }
-
-    return "$CACHEDIR/build.$tree.$host.$compiler";
+	my ($self, $tree, $host, $compiler, $rev) = @_;
+	if ($rev) {
+		return "$CACHEDIR/oldrevs/build.$tree.$host.$compiler-$rev";
+	}
+	return "$CACHEDIR/build.$tree.$host.$compiler";
 }
 
 ################################
 # get the name of the build file
 sub build_fname($$$$$)
 {
-    my ($self, $tree, $host, $compiler, $rev) = @_;
-    if ($rev) {
-	    return "$self->{path}/oldrevs/build.$tree.$host.$compiler-$rev";
-    }
-    return "$self->{path}/build.$tree.$host.$compiler";
+	my ($self, $tree, $host, $compiler, $rev) = @_;
+	if ($rev) {
+		return "$self->{path}/oldrevs/build.$tree.$host.$compiler-$rev";
+	}
+	return "$self->{path}/build.$tree.$host.$compiler";
 }
 
 ###################
@@ -93,16 +92,16 @@ sub build_fname($$$$$)
 sub build_age_mtime($$$$$)
 {
 	my ($self, $host, $tree, $compiler, $rev) = @_;
-    my $file=$self->build_fname($tree, $host, $compiler, $rev);
-    my $age = -1;
-    my $st;
+	my $file=$self->build_fname($tree, $host, $compiler, $rev);
+	my $age = -1;
+	my $st;
 
-    $st = stat("$file.log");
-    if ($st) {
+	$st = stat("$file.log");
+	if ($st) {
 		$age = time() - $st->mtime;
-    }
+	}
 
-    return $age;
+	return $age;
 }
 
 ##############################################
@@ -110,16 +109,16 @@ sub build_age_mtime($$$$$)
 sub build_age_ctime($$$$$)
 {
 	my ($self, $host, $tree, $compiler, $rev) = @_;
-    my $file = $self->build_fname($tree, $host, $compiler, $rev);
-    my $age = -1;
-    my $st;
+	my $file = $self->build_fname($tree, $host, $compiler, $rev);
+	my $age = -1;
+	my $st;
 
-    $st = stat("$file.log");
-    if ($st) {
+	$st = stat("$file.log");
+	if ($st) {
 		$age = time() - $st->ctime;
-    }
+	}
 
-    return $age;
+	return $age;
 }
 
 ##############################################
@@ -127,45 +126,45 @@ sub build_age_ctime($$$$$)
 sub build_revision_details($$$$$)
 {
 	my ($self, $host, $tree, $compiler, $rev) = @_;
-    my $file = $self->build_fname($tree, $host, $compiler, $rev);
+	my $file = $self->build_fname($tree, $host, $compiler, $rev);
 	my $cachef = $self->cache_fname($tree, $host, $compiler, $rev);
-    my $log;
-    my $ret = 0;
+	my $log;
+	my $ret = 0;
 
-    # don't fast-path for trees with git repository:
-    # we get the timestamp as rev and want the details
-    if ($rev && ($tree ne "samba_3_2_test")) {
-	    return $rev;
-    }
+	# don't fast-path for trees with git repository:
+	# we get the timestamp as rev and want the details
+	if ($rev && ($tree ne "samba_3_2_test")) {
+		return $rev;
+	}
 
-    my $st1 = stat("$file.log");
+	my $st1 = stat("$file.log");
 
-    if (!$st1) {
-	    return $ret;
-    }
-    my $st2 = stat("$cachef.revision");
+	if (!$st1) {
+		return $ret;
+	}
+	my $st2 = stat("$cachef.revision");
 
-    # the ctime/mtime asymmetry is needed so we don't get fooled by
-    # the mtime update from rsync 
-    if ($st1 && $st2 && $st1->ctime <= $st2->mtime) {
-	    return util::FileLoad("$cachef.revision");
-    }
+	# the ctime/mtime asymmetry is needed so we don't get fooled by
+	# the mtime update from rsync 
+	if ($st1 && $st2 && $st1->ctime <= $st2->mtime) {
+		return util::FileLoad("$cachef.revision");
+	}
 
-    $log = util::FileLoad("$file.log");
+	$log = util::FileLoad("$file.log");
 
-    if ($log =~ /BUILD COMMIT REVISION: (.*)/) {
-	$ret = $1;
-    } elsif ($log =~ /BUILD REVISION: (.*)/) {
-	$ret = $1;
-    }
+	if ($log =~ /BUILD COMMIT REVISION: (.*)/) {
+		$ret = $1;
+	} elsif ($log =~ /BUILD REVISION: (.*)/) {
+		$ret = $1;
+	}
 
-    if ($log =~ /BUILD COMMIT TIME: (.*)/) {
-	$ret .= ":".$1;
-    }
+	if ($log =~ /BUILD COMMIT TIME: (.*)/) {
+		$ret .= ":".$1;
+	}
 
-    util::FileSave("$cachef.revision", $ret);
+	util::FileSave("$cachef.revision", $ret);
 
-    return $ret;
+	return $ret;
 }
 
 sub build_revision($$$$$)
@@ -194,149 +193,148 @@ sub build_revision_time($$$$$)
 # get status of build
 sub build_status($$$$$)
 {
-    my ($self, $host, $tree, $compiler, $rev) = @_;
-    my $file = $self->build_fname($tree, $host, $compiler, $rev);
-    my $cachefile = $self->cache_fname($tree, $host, $compiler, $rev).".status";
-    my ($cstatus, $bstatus, $istatus, $tstatus, $sstatus, $dstatus, $tostatus);
-    $cstatus = $bstatus = $istatus = $tstatus = $sstatus = $dstatus = $tostatus =
-      span({-class=>"status unknown"}, "?");
+	my ($self, $host, $tree, $compiler, $rev) = @_;
+	my $file = $self->build_fname($tree, $host, $compiler, $rev);
+	my $cachefile = $self->cache_fname($tree, $host, $compiler, $rev).".status";
+	my ($cstatus, $bstatus, $istatus, $tstatus, $sstatus, $dstatus, $tostatus);
+	$cstatus = $bstatus = $istatus = $tstatus = $sstatus = $dstatus = $tostatus =
+		span({-class=>"status unknown"}, "?");
 
-    my $st1 = stat("$file.log");
-    if (!$st1) {
-	    return "Unknown Build";
-    }
-    my $st2 = stat($cachefile);
-
-    if ($st1 && $st2 && $st1->ctime <= $st2->mtime) {
-		return util::FileLoad($cachefile);
-    }
-
-    my $log = util::FileLoad("$file.log");
-    my $err = util::FileLoad("$file.err");
-
-    sub span_status($)
-    {
-	my $st = shift;
-	if ($st == 0) {
-		return span({-class=>"status passed"}, "ok");
-	} else {
-		return span({-class=>"status failed"}, $st);
+	my $st1 = stat("$file.log");
+	if (!$st1) {
+		return "Unknown Build";
 	}
-    }
+	my $st2 = stat($cachefile);
 
-    if ($log =~ /ACTION FAILED: test/) {
+	if ($st1 && $st2 && $st1->ctime <= $st2->mtime) {
+		return util::FileLoad($cachefile);
+	}
+
+	my $log = util::FileLoad("$file.log");
+	my $err = util::FileLoad("$file.err");
+
+	sub span_status($)
+	{
+		my $st = shift;
+		if ($st == 0) {
+			return span({-class=>"status passed"}, "ok");
+		} else {
+			return span({-class=>"status failed"}, $st);
+		}
+	}
+
+	if ($log =~ /ACTION FAILED: test/) {
 		$tstatus = span_status(255);
-    }
-    
-    if ($log =~ /TEST STATUS:(.*)/) {
+	}
+
+	if ($log =~ /TEST STATUS:(.*)/) {
 		$tstatus = span_status($1);
-    }
-    
-    if ($log =~ /INSTALL STATUS:(.*)/) {
+	}
+
+	if ($log =~ /INSTALL STATUS:(.*)/) {
 		$istatus = span_status($1);
-    }
-    
-    if ($log =~ /BUILD STATUS:(.*)/) {
+	}
+
+	if ($log =~ /BUILD STATUS:(.*)/) {
 		$bstatus = span_status($1);
-    }
+	}
 
-    if ($log =~ /CONFIGURE STATUS:(.*)/) {
+	if ($log =~ /CONFIGURE STATUS:(.*)/) {
 		$cstatus = span_status($1);
-    }
-    
-    if ($log =~ /(PANIC|INTERNAL ERROR):.*/ ) {
-	$sstatus = "/".span({-class=>"status panic"}, "PANIC");
-    } else {
-	$sstatus = "";
-    }
+	}
 
-    if ($err =~ /No space left on device.*/ ) {
-	$dstatus = "/".span({-class=>"status failed"}, "disk full");
-    } elsif ($log =~ /No space left on device.*/ ) {
-	$dstatus = "/".span({-class=>"status failed"}, "disk full");
-    } else {
-	$dstatus = "";
-    }
+	if ($log =~ /(PANIC|INTERNAL ERROR):.*/ ) {
+		$sstatus = "/".span({-class=>"status panic"}, "PANIC");
+	} else {
+		$sstatus = "";
+	}
 
-    if ($log =~ /maximum runtime exceeded.*/ ) {
-	$tostatus = "/".span({-class=>"status failed"}, "timeout");    
-    } else {
-    	$tostatus = "";
-    }
+	if ($err =~ /No space left on device.*/ ) {
+		$dstatus = "/".span({-class=>"status failed"}, "disk full");
+	} elsif ($log =~ /No space left on device.*/ ) {
+		$dstatus = "/".span({-class=>"status failed"}, "disk full");
+	} else {
+		$dstatus = "";
+	}
 
-    if ($log =~ /CC_CHECKER STATUS: (.*)/ && $1 > 0) {
-	$sstatus .= "/".span({-class=>"status checker"}, $1);
-    }
+	if ($log =~ /maximum runtime exceeded.*/ ) {
+		$tostatus = "/".span({-class=>"status failed"}, "timeout");    
+	} else {
+		$tostatus = "";
+	}
 
-    my $ret = "$cstatus/$bstatus/$istatus/$tstatus$sstatus$dstatus$tostatus";
+	if ($log =~ /CC_CHECKER STATUS: (.*)/ && $1 > 0) {
+		$sstatus .= "/".span({-class=>"status checker"}, $1);
+	}
 
-    util::FileSave("$cachefile", $ret);
+	my $ret = "$cstatus/$bstatus/$istatus/$tstatus$sstatus$dstatus$tostatus";
 
-    return $ret;
+	util::FileSave("$cachefile", $ret);
+
+	return $ret;
 }
 
 ##############################################
 # get status of build
 sub lcov_status($$)
 {
-    my ($self, $tree) = @_;
-    my $cachefile="$CACHEDIR/lcov.$LCOVHOST.$tree.status";
-    my $file = "$LCOVDIR/$LCOVHOST/$tree/index.html";
-    my $st1 = stat($file);
-    if (!$st1) {
-	    return "";
-    }
-    my $st2 = stat($cachefile);
+	my ($self, $tree) = @_;
+	my $cachefile="$CACHEDIR/lcov.$LCOVHOST.$tree.status";
+	my $file = "$LCOVDIR/$LCOVHOST/$tree/index.html";
+	my $st1 = stat($file);
+	if (!$st1) {
+		return "";
+	}
+	my $st2 = stat($cachefile);
 
-    if ($st1 && $st2 && $st1->ctime <= $st2->mtime) {
-	return util::FileLoad($cachefile);
-    }
+	if ($st1 && $st2 && $st1->ctime <= $st2->mtime) {
+		return util::FileLoad($cachefile);
+	}
 
-    my $ret;
-    my $lcov_html = util::FileLoad($file);
-    if ($lcov_html =~ /\<td class="headerItem".*?\>Code\&nbsp\;covered\:\<\/td\>.*?\n.*?\<td class="headerValue".*?\>([0-9.]+) \%/) {
-    
-	$ret = '<a href="/lcov/data/'."$LCOVHOST/$tree\">$1 %</a>";
-    }  else {
-	$ret = "";
-    }
-    util::FileSave("$cachefile", $ret);
-    return $ret;
+	my $ret;
+	my $lcov_html = util::FileLoad($file);
+	if ($lcov_html =~ /\<td class="headerItem".*?\>Code\&nbsp\;covered\:\<\/td\>.*?\n.*?\<td class="headerValue".*?\>([0-9.]+) \%/) {
+		$ret = '<a href="/lcov/data/'."$LCOVHOST/$tree\">$1 %</a>";
+	} else {
+		$ret = "";
+	}
+	util::FileSave("$cachefile", $ret);
+	return $ret;
 }
 
 ##############################################
 # get status of build
 sub err_count($$$$$)
 {
-    my ($self, $host, $tree, $compiler, $rev) = @_;
-    my $file = $self->build_fname($tree, $host, $compiler, $rev);
+	my ($self, $host, $tree, $compiler, $rev) = @_;
+	my $file = $self->build_fname($tree, $host, $compiler, $rev);
 	my $cachef = $self->cache_fname($tree, $host, $compiler, $rev);
-    my $err;
+	my $err;
 
-    my $st1 = stat("$file.err");
-    if ($st1) {
-	    return 0;
-    }
-    my $st2 = stat("$cachef.errcount");
+	my $st1 = stat("$file.err");
+	if ($st1) {
+		return 0;
+	}
+	my $st2 = stat("$cachef.errcount");
 
-    if ($st1 && $st2 && $st1->ctime <= $st2->mtime) {
-	    return util::FileLoad("$cachef.errcount");
-    }
+	if ($st1 && $st2 && $st1->ctime <= $st2->mtime) {
+		return util::FileLoad("$cachef.errcount");
+	}
 
-    $err = util::FileLoad("$file.err") or return 0;
+	$err = util::FileLoad("$file.err") or return 0;
 
-    my $ret = util::count_lines($err);
+	my $ret = util::count_lines($err);
 
-    util::FileSave("$cachef.errcount", "$ret");
+	util::FileSave("$cachef.errcount", "$ret");
 
-    return $ret;
+	return $ret;
 }
 
 ##############################################
 # read full log file
 sub read_log($$$$$)
 {
-    my ($self, $tree, $host, $compiler, $rev) = @_;
+	my ($self, $tree, $host, $compiler, $rev) = @_;
 
 	return util::FileLoad($self->build_fname($tree, $host, $compiler, $rev).".log");
 }
@@ -345,7 +343,7 @@ sub read_log($$$$$)
 # read full err file
 sub read_err($$$$$)
 {
-    my ($self, $tree, $host, $compiler, $rev) = @_;
+	my ($self, $tree, $host, $compiler, $rev) = @_;
 
 	return util::FileLoad($self->build_fname($tree, $host, $compiler, $rev).".err");
 }
@@ -354,17 +352,17 @@ sub read_err($$$$$)
 # get a list of old builds and their status
 sub get_old_revs($$$$)
 {
-    my ($self, $tree, $host, $compiler) = @_;
-    my @list = split('\n', `ls $self->{path}/oldrevs/build.$tree.$host.$compiler-*.log`);
-    my %ret;
-    for my $l (@list) {
-	    if ($l =~ /-(\d+).log$/) {
-		    my $rev = $1;
-		    $ret{$rev} = $self->build_status($host, $tree, $compiler, $rev);
-	    }
-    }
+	my ($self, $tree, $host, $compiler) = @_;
+	my @list = split('\n', `ls $self->{path}/oldrevs/build.$tree.$host.$compiler-*.log`);
+	my %ret;
+	for my $l (@list) {
+		if ($l =~ /-(\d+).log$/) {
+			my $rev = $1;
+			$ret{$rev} = $self->build_status($host, $tree, $compiler, $rev);
+		}
+	}
 
-    return %ret;
+	return %ret;
 }
 
 sub has_host($$)
