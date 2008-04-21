c20699b8c8ada50cb30d020a7ad5389df48e4786
jelmer <jelmer@1e5ffdc8-eadd-0310-9daa-9cb4117fe24b>
2008-04-21 10:03:36 +0000

    Add functions for creating rsyncd.secrets and the host list. (Pair programmed with Andrew)
    git-svn-id: file:///home/svn/build-farm/trunk@775 1e5ffdc8-eadd-0310-9daa-9cb4117fe24b

--- tests/hostdb.pl
+++ tests/hostdb.pl
@@ -5,18 +5,20 @@ use FindBin qw($RealBin);
 use lib "$RealBin/..";
 use lib "$RealBin/../web";
 
-use Test::More tests => 6;
+use Test::More tests => 7;
 use strict;
 use hostdb;
 
 # make sure that provisioning adds the right table.
-my $db = new hostdb("foo.sqlite");
+my $db = new hostdb("dbname=:memory:");
 ok($db->provision());
 is_deeply([], $db->{dbh}->selectall_arrayref("SELECT * FROM host"));
 
 ok($db->createhost("gwalcmai", "vax", "jelmer", "jelmer\@example.com", "geheim", "Yo! Please put me on the buildfarm"));
 
 is_deeply([["gwalcmai"]], $db->{dbh}->selectall_arrayref("SELECT name FROM host"));
+
+is_deeply([{ name => "gwalcmai" }], $db->hosts());
 
 ok($db->deletehost("gwalcmai"));
 is_deeply([], $db->{dbh}->selectall_arrayref("SELECT name FROM host"));
