691363cc2da79306b8050aa1def2a7fefedad699
build <build@1e5ffdc8-eadd-0310-9daa-9cb4117fe24b>
2009-10-28 04:58:29 +0000

    Rework 'daily.sh' script for 'new' (as of a few months ago) build farm layout
    Andrew Bartlett
    
    
    git-svn-id: file:///home/svn/build-farm/trunk@910 1e5ffdc8-eadd-0310-9daa-9cb4117fe24b

--- daily.sh
+++ daily.sh
@@ -4,14 +4,14 @@ sqlite3 `dirname $0`/hostdb.sqlite 'VACUUM;'
 cd `dirname $0` && exec ./mail-dead-hosts.pl
 
 # delete old ones that are not used any more
-find oldrevs -type f -mtime +4 -links 1 -print0 | xargs -i -0 rm -f \{\}
+find `dirname $0`/data/oldrevs -type f -mtime +4 -links 1 -print0 | xargs -i -0 rm -f \{\}
 
 # delete any really old data
-find . -type f -mtime +120  -print0 | xargs -i -0 rm -f \{\}
+find `dirname $0`/data -type f -mtime +120  -print0 | xargs -i -0 rm -f \{\}
 
 # delete old cache data
-find ../cache -type f -name "build.*" -mtime +1 -print0 | xargs -i -0 rm -f \{\}
+find `dirname $0`/cache -type f -name "build.*" -mtime +1 -print0 | xargs -i -0 rm -f \{\}
 
 # delete partially uploaded files (crashed rsync)
-find . -type f -mtime +2 -name ".build.*" -print0 | xargs -i -0 rm -f \{\}
+find `dirname $0`/data/upload -type f -mtime +2 -name ".build.*" -print0 | xargs -i -0 rm -f \{\}
 
