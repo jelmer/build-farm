bddf87d076c7bc31e2e91aacf1c3d8522d42d0f9
build <build@1e5ffdc8-eadd-0310-9daa-9cb4117fe24b>
2009-12-29 23:17:08 +0000

    don't keep old builds quite as long
    git-svn-id: file:///home/svn/build-farm/trunk@928 1e5ffdc8-eadd-0310-9daa-9cb4117fe24b

--- daily.sh
+++ daily.sh
@@ -4,7 +4,7 @@ sqlite3 `dirname $0`/hostdb.sqlite 'VACUUM;'
 cd `dirname $0` && exec ./mail-dead-hosts.pl
 
 # delete old ones that are not used any more
-find `dirname $0`/data/oldrevs -type f -mtime +21 -links 1 -print0 | xargs -i -0 rm -f \{\}
+find `dirname $0`/data/oldrevs -type f -mtime +10 -links 1 -print0 | xargs -i -0 rm -f \{\}
 
 # delete any really old data
 find `dirname $0`/data -type f -mtime +120  -print0 | xargs -i -0 rm -f \{\}
