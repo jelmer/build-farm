cf59e476b932d09274a3cb22c8719cc835e2fa14
build <build@1e5ffdc8-eadd-0310-9daa-9cb4117fe24b>
2010-03-08 11:26:53 +0000

    fixed daily.sh
    git-svn-id: file:///home/svn/build-farm/trunk@938 1e5ffdc8-eadd-0310-9daa-9cb4117fe24b

--- daily.sh
+++ daily.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-sqlite3 `dirname $0`/hostdb.sqlite 'VACUUM;'
-cd `dirname $0` && exec ./mail-dead-hosts.pl
+cd $HOME/master || exit 1
+set -x
 
-# delete old ones that are not used any more
+(
+date
+set -x
+sqlite3 `dirname $0`/hostdb.sqlite 'VACUUM;'
+cd `dirname $0` && ./mail-dead-hosts.pl
+
+echo "deleting old file that are not used any more"
 find `dirname $0`/data/oldrevs -type f -mtime +10 -links 1 -print0 | xargs -i -0 rm -f \{\}
 
-# delete any really old data
+echo "deleting any really old data"
 find `dirname $0`/data -type f -mtime +120  -print0 | xargs -i -0 rm -f \{\}
 
-# delete old cache data
+echo "delete old cache data"
 find `dirname $0`/cache -type f -name "build.*" -mtime +1 -print0 | xargs -i -0 rm -f \{\}
 
-# delete partially uploaded files (crashed rsync)
+echo "delete partially uploaded files (crashed rsync)"
 find `dirname $0`/data/upload -type f -mtime +2 -name ".build.*" -print0 | xargs -i -0 rm -f \{\}
 
+) >> daily.log 2>&1
