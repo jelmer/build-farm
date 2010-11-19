7596b6699b6b29dcc8701dd54e73a0770fb9c89f
Jelmer Vernooij <jelmer@samba.org>
2010-11-19 19:34:58 +0100

    Store basename in database.

--- buildfarm/sqldb.py
+++ buildfarm/sqldb.py
@@ -55,6 +55,7 @@ class StormBuild(Build):
     upload_time = Int(name="age")
     status_str = RawStr(name="status")
     commit_revision = RawStr()
+    basename = RawStr()
 
     def status(self):
         return BuildStatus.__deserialize__(self.status_str)
@@ -206,6 +207,7 @@ class StormCachingBuildResultStore(BuildResultStore):
         new_build.checksum = build.log_checksum()
         new_build.upload_time = build.upload_time
         new_build.status_str = build.status().__serialize__()
+        new_build.basename = new_basename
         self.store.add(new_build)
         return new_build
 
@@ -263,7 +265,7 @@ class StormCachingBuildFarm(BuildFarm):
 def setup_schema(db):
     db.execute("CREATE TABLE IF NOT EXISTS host (id integer primary key autoincrement, name blob, owner text, owner_email text, password text, ssh_access int, fqdn text, platform text, permission text, last_dead_mail int, join_time int);", noresult=True)
     db.execute("CREATE UNIQUE INDEX IF NOT EXISTS unique_hostname ON host (name);", noresult=True)
-    db.execute("CREATE TABLE IF NOT EXISTS build (id integer primary key autoincrement, tree blob, revision blob, host blob, compiler blob, checksum blob, age int, status blob, commit_revision blob);", noresult=True)
+    db.execute("CREATE TABLE IF NOT EXISTS build (id integer primary key autoincrement, tree blob, revision blob, host blob, compiler blob, checksum blob, age int, status blob, commit_revision blob, basename blob);", noresult=True)
     db.execute("CREATE UNIQUE INDEX IF NOT EXISTS unique_checksum ON build (checksum);", noresult=True)
 
 
